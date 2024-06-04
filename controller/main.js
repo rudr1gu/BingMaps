bcc7eeca6a099f8a741f2bd91ab0f1247a220ac2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-06-04 14:30:29 -0300

    add funções

--- controller/main.js
+++ controller/main.js
@@ -1,15 +1,15 @@
 let map;
-let pin; 
+let pin;
+
+const btnPosition = document.getElementById('btnGetPosition');
+let local = document.getElementById('local');
+let result = document.getElementById('result');
 
 function GetMap(){
     map = new Microsoft.Maps.Map('#myMap',{
-        //adiciona a localização com as cordenadas
         center: new Microsoft.Maps.Location(-23.502098271488702, -46.420680231192996),
-        //tipo de mapa para imagen aereas "aerial" e para rua "street"
          mapTypeId: Microsoft.Maps.MapTypeId.street,
          zoom: 20,
-
-         //editar cores do mapa
          customMapStyle: {
             elements: {
                 area: { fillColor: '#b6e591' },
@@ -26,6 +26,18 @@ function GetMap(){
         }
     });
 
+    btnPosition.addEventListener('click', () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            map.setView({center: new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude), zoom: 20});
+            
+            let pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
+            map.entities.push(pushpin, {visible: true, draggable: true, anchor: new Microsoft.Maps.Point(0, 0)});
+            local.innerHTML = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`; 
+            result.style.display = 'flex';          
+        });
+    });
+
     Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', function () {
         var options = {
             maxResults: 4,
@@ -34,74 +46,16 @@ function GetMap(){
         var manager = new Microsoft.Maps.AutosuggestManager(options);
         manager.attachAutosuggest('#searchBox', '#searchBoxContainer', selectedSuggestion);
     });
+
     function selectedSuggestion(suggestionResult) {
         map.entities.clear();
         map.setView({ bounds: suggestionResult.bestView });
         var pushpin = new Microsoft.Maps.Pushpin(suggestionResult.location);
         map.entities.push(pushpin);
     }
-    
+};
 
-    
-    //requerindo os enderços do arquivo .json 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', './modulo/enderecos.json', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var enderecos = JSON.parse(xhr.responseText);
-            addPushpins(enderecos);
-        }
-    };
-    xhr.send();    
-
-    //utilizando um laço para adicionar marcadores em diferentes endereços
-    let addPushpins = (enderecos) =>{
-        for(let endereco of enderecos){     
-            pin = new Microsoft.Maps.Pushpin(endereco.location,{
-                title: endereco.title,
-                subTitle: endereco.subTitle,
-                icon: endereco.icon
-            });
-
-            
-            //informaçõe do pushpin
-            pin.metadata = {
-                title: endereco.title,
-                description: endereco.description 
-            };
-            
-            infobox = new Microsoft.Maps.Infobox(pin, {visible: false})
-
-            infobox.setMap(map)
-            map.entities.push(pin);
-
-            Microsoft.Maps.Events.addHandler(pin, 'click', pushpinClicked);
-
-            //Criar uma função on click no PushPin
-            // Microsoft.Maps.Events.addHandler(pin, "click", function() {
-            //     highlight('pushpinClick');
-            // });
-        }   
-    }
-
-    
-    //função para adicioar clique no marcador pushpin
-    function pushpinClicked(e) {
-        //Make sure the infobox has metadata to display.
-        if (e.target.metadata) {
-            //Set the infobox options with the metadata of the pushpin.
-            infobox.setOptions({
-                location: e.target.getLocation(),
-                title: e.target.metadata.title,
-                description: e.target.metadata.description,
-                visible: true
-            });
-        }
-    }
-
-}
-
-// function highlight(id) {
-//     document.getElementById(id).style.display = 'flex';
-// }
-
+const close = document.getElementById('close');
+close.addEventListener('click', () => {
+    result.style.display = 'none';
+});
--- views/estilo.js
+++
@@ -1,6 +0,0 @@
-const fclose = document.getElementById("close")
-const pushPin = document.getElementById("pushpinClick")
-
-fclose.addEventListener("click", ()=>{
-    pushPin.style.display = "none"
-})
